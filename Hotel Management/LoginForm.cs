5572a06e4f1728e1cdd33f0a1d71303912d45c1d
BeriiiOsu <157445482+BeriiiOsu@users.noreply.github.com>
2025-08-05 16:46:22 +0800

    Update 4:46pm 05/08/2025

---
+++ Hotel Management/LoginForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hotel_Management
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            if(txtPassword.Text.Length != 0)
+            {
+                btnShow.Visible = true;
+            }
+            else
+            {
+                btnShow.Visible = false;
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = !txtPassword.UseSystemPasswordChar;
+        }
+    }
+}
